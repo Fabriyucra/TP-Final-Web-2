47de7c496b44d310a9ef0898659b10cde94b8509
fabricioy <fabriyucra@gmail.com>
2020-12-09 17:04:53 -0300

    header modificado

--- source/views/shared/_header.php
+++ source/views/shared/_header.php
@@ -1,5 +1,5 @@
-<nav class="navbar navbar-inverse navbar-global navbar-static-top w3-large">
-	<div class="container">
+
+    <div class="w3-bar w3-white w3-wide w3-padding w3-card">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbarContenido" aria-expanded="false">
 				<span class="sr-only">Abrir navegación</span>
@@ -7,17 +7,16 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a href="index.php" class="navbar-brand">CD&U Logistica</a>
+			<a href="index.php" class="w3-bar-item w3-button"> <b>CD&U</b> Logistica</a>
 		</div>
 		<div id="navbarContenido" class="collapse navbar-collapse">
-			<div class="navbar-text">
+			<div class="w3-bar-item">
 				<i class="material-icons">perm_identity</i>
 				<strong>Bienvenido, <?php echo $_SESSION['nombre']; ?></strong>
 			</div>
-			<ul class="nav navbar-nav navbar-right">
+			<ul class=" w3-button w3-right">
 
-				<li><a href="salir.php"><i class="material-icons">settings_power</i>Salir</a></li>
+				<li><a href="Salir.php"><i class="material-icons">settings_power</i>Salir</a></li>
 			</ul>
 		</div>
 	</div>
-</nav>
